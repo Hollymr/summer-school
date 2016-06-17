1223e01f2d1eeddde227abd17eec5e4cd6ea9a21
Holly Rhodes <hllyrhodes@gmail.com>
2016-06-17 16:11:25 -0400

    First return for Enroll

--- SummerSchool/Program.cs
+++ SummerSchool/Program.cs
@@ -10,31 +10,44 @@ namespace SummerSchool
     {
         static string[] Students = new string[15];
 
-        static void Main(string[] args)
+        static int MenuGetInput()
         {
+            
+            int menuChoice = 1;
+            
+
             Console.WriteLine("Welcome to Hogwarts!");
             Console.WriteLine("Choose option below.");
-           
-            while(true)
-            {
-                // Print out the menu 
-                Console.WriteLine("1. Enroll a student");
-                Console.WriteLine("2. Unenroll a student");
-                Console.WriteLine("3. Print out list of enrolled students");
-                Console.WriteLine("4. Exit");
 
-                int choice = Convert.ToInt32(Console.ReadLine());
-                if (choice == 1)
-                {
-                    EnrollStudent();
-                }
+            Console.WriteLine("1. Enroll a student");
+            Console.WriteLine("2. Unenroll a student");
+            Console.WriteLine("3. Print out list of enrolled students");
+            Console.WriteLine("4. Exit");
 
-            }
+            int mainChoice = Convert.ToInt32(Console.ReadLine());
+            return menuChoice;
+
             
         }
+        
+
+        static void Main(string[] args)
+        {
+            int menuChoice = MenuGetInput();
+            if (menuChoice == 1) 
+            {
+                EnrollStudent();
+            }
+            Console.ReadKey();       
+        }
+
+
+         
+
         static void EnrollStudent()
         {
-            Students[1] = "Tom";
+            //Students[1] = "Tom";
+            Console.WriteLine("Enroll");
             
 
         }
@@ -52,8 +65,14 @@ namespace SummerSchool
 
         static void Exit()
         {
+
+
             Console.ReadKey();
+                
         }
+
+        
+    
 
     }
     
