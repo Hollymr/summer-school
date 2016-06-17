17b10863f1986ce45e4f3b74f0a84f8c5cd3fcc4
Holly Rhodes <hllyrhodes@gmail.com>
2016-06-17 15:30:05 -0400

    Hogwarts Menu

--- SummerSchool/Program.cs
+++ SummerSchool/Program.cs
@@ -8,8 +8,53 @@ namespace SummerSchool
 {
     class Program
     {
+        static string[] Students = new string[15];
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Hogwarts!");
+            Console.WriteLine("Choose option below.");
+           
+            while(true)
+            {
+                // Print out the menu 
+                Console.WriteLine("1. Enroll a student");
+                Console.WriteLine("2. Unenroll a student");
+                Console.WriteLine("3. Print out list of enrolled students");
+                Console.WriteLine("4. Exit");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 1)
+                {
+                    EnrollStudent();
+                }
+
+            }
+            
         }
+        static void EnrollStudent()
+        {
+            Students[1] = "Tom";
+            
+
+        }
+
+        static void UnEnrollStudents()
+        {
+        
+
+        }
+
+        static void PrintStudentList()
+        {
+
+        }
+
+        static void Exit()
+        {
+            Console.ReadKey();
+        }
+
     }
+    
 }
