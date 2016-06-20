409b5216413b292f1c25eb75f4c404eb219deefc
Holly Rhodes <hllyrhodes@gmail.com>
2016-06-20 11:34:36 -0400

    fixed enroll student, get spot, and print list

--- SummerSchool/Program.cs
+++ SummerSchool/Program.cs
@@ -79,39 +79,20 @@ namespace SummerSchool
                 {
                     return i;
                 }
-                return -1;
+                
             }
-        
+            return -1;
         }
 
 
 
         static void EnrollStudent()
         {
-
-
-            //string[] students = new string[15];
-
-
-            for (int i = 0; i < 15; i++)
-            {
-                Console.WriteLine("Enter a student then press enter.");
-                students[i] = Console.ReadLine();
-                
-               
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("You just enrolled: ");
-            for (int i = 0; i < 15; i++)
-            {
-                Console.WriteLine(students[i]);
-                 
-
-            }
-
-
-
+ 
+            Console.WriteLine("Enter a student then press enter.");
+            string student = Console.ReadLine();
+            int spot = GetAvalibleSpot();
+            students[spot] = student;
         }
 
         static void UnEnrollStudent()
@@ -136,14 +117,11 @@ namespace SummerSchool
 
         static void PrintStudentList()
         {
-            //string[] students = new string[15];
-
-            Console.WriteLine();
-            Console.WriteLine("Student List");
-           for (int i = 0; i < students.Length; i++)
+           
+            for (int i = 0; i < students.Length; i++)
             {
                 // EnrollStudent()
-                Console.WriteLine(students[1]);
+                Console.WriteLine(students[i]);
             }
         }
 
