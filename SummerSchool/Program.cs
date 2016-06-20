3c0c3408092f5ee47a92acf8155f7301818dddac
Holly Rhodes <hllyrhodes@gmail.com>
2016-06-19 22:01:17 -0400

    added names to enroll

--- SummerSchool/Program.cs
+++ SummerSchool/Program.cs
@@ -8,7 +8,7 @@ namespace SummerSchool
 {
     class Program
     {
-        static string[] Students = new string[15];
+        //static string[] Students = new string[15];
 
         static int MenuGetInput()
         {
@@ -18,28 +18,32 @@ namespace SummerSchool
             
 
             Console.WriteLine("Welcome to Hogwarts!");
+            Console.WriteLine();
             Console.WriteLine("Choose option below.");
-
+            Console.WriteLine();
             Console.WriteLine("1. Enroll a student");
             Console.WriteLine("2. Unenroll a student");
             Console.WriteLine("3. Print out list of enrolled students");
             Console.WriteLine("4. Exit");
-
+            
             int menuChoice = Convert.ToInt32(Console.ReadLine());
             return menuChoice;
 
             
         }
-        
+
+        //atic string[] students = new string[15];
+            
 
         static void Main(string[] args)
         {
-            while (true)
+           //hile(true)
             {
+                
                 int menuChoice = MenuGetInput();
-                if (menuChoice == 1)
+                if (menuChoice == 1)                    
                 {
-                    EnrollStudent();
+                   EnrollStudent();                   
                 }
 
                 if (menuChoice == 2)
@@ -64,14 +68,34 @@ namespace SummerSchool
 
 
 
-         
+
 
         static void EnrollStudent()
         {
-            //Students[1] = "Tom";
-            Console.WriteLine("Enroll");
-            
 
+
+            string[] students = new string[15];
+
+
+            for (int i = 0; i <= 15; i++)
+            {
+                Console.WriteLine("Enter a student then press enter.");
+                students[i] = Console.ReadLine();
+
+                if (i == 15)
+                    continue; 
+               
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("You just enrolled: ");
+             for (int i= 0; i <= 15; i++)
+            {
+                Console.WriteLine(students[i]);
+            }
+
+
+       
         }
 
         static void UnEnrollStudent()
